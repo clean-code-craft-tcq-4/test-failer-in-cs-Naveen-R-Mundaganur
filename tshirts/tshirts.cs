23d9bf4f3407a491e23fe42a6eed337190c621b3
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-06-23 12:20:58 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace TshirtSpace {
-    class Tshirt {
-        static string Size(int cms) {
-            if(cms < 38) {
+namespace TshirtSpace 
+{
+    class Tshirt 
+    {
+        static string Size(int cms) 
+        {
+            if(cms < 38) 
+            {
                 return "S";
-            } else if(cms > 38 && cms < 42) {
+            } 
+            else if(cms > 38 && cms < 42) 
+            {
                 return "M";
-            } else {
+            } 
+            else 
+            {
                 return "L";
             }
         }
-        static void Main(string[] args) {
-            Debug.Assert(Size(37) == "S");
-            Debug.Assert(Size(40) == "M");
+        static void Main(string[] args) 
+        {
+            Debug.Assert(Size(38) == "S");
+            Debug.Assert(Size(42) == "M");            
             Debug.Assert(Size(43) == "L");
+            Debug.Assert(Size(null) == "L");
             Console.WriteLine( "All is well (maybe!)");
         }
     }
