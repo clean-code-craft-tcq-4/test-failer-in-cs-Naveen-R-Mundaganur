93b0772bf090e7227944e6fd827354c4f6d17e32
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-06-23 14:06:04 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -7,19 +7,19 @@ namespace AlerterSpace
     {
         static int alertFailureCount = 0;
         static int networkAlertStub(float celcius) 
-        {
-            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
-            if(celcius<=200)
+        {            
+            // Return 200 for ok
+            // Return 500 for not-ok
+            // stub always succeeds and returns 200  
+            if (celcius <= 200)
             {
                 return 200;
             }
             else
             {
+                Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
                 return 500;
-            }
-            // Return 200 for ok
-            // Return 500 for not-ok
-            // stub always succeeds and returns 200       
+            }                 
         }
         static void alertInCelcius(float farenheit) 
         {
