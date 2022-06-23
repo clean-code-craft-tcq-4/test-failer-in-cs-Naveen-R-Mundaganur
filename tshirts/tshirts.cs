076b8d3418ca43effea51a37977058229a1571a4
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-06-23 12:48:23 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -9,18 +9,29 @@ namespace TshirtSpace
         {
             if(cms!=null)
             {
-                if(cms < 38) 
+                if(cms>0)
                 {
-                    return "S";
-                } 
-                else if(cms > 38 && cms < 42) 
-                {
-                    return "M";
-                } 
-                else 
-                {
-                    return "L";
+                    if(cms < 38) 
+                    {
+                        return "S";
+                    } 
+                    else if(cms > 38 && cms < 42) 
+                    {
+                        return "M";
+                    } 
+                    else 
+                    {
+                        return "L";
+                    }
                 }
+                else
+                {
+                    return "Invalid Choice"
+                }
+            }
+            else
+            {
+                 return null;
             }
         }
         static void Main(string[] args) 
@@ -28,6 +39,7 @@ namespace TshirtSpace
             Debug.Assert(Size(38) == "S");
             Debug.Assert(Size(42) == "M");            
             Debug.Assert(Size(43) == "L");
+            Debug.Assert(Size(0)=="Invalid Choice");
             Debug.Assert(Size(null) == null);
             Console.WriteLine( "All is well (maybe!)");
         }
